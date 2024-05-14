cdddaff7d33c791094708291cdce08c5a66b094b
NguyenAnhToi <toidxbp02gmail.com>
2024-05-14 14:42:30 +0700

    Bài 25 Funtion

--- Day-1/index.php
+++ Day-1/index.php
@@ -1,10 +1,13 @@
 <?php
-//$a = 10;
-if(isset($a)){
-    echo "True";
-}
-else{
-    echo "False";
+function thongtin($ten,$namsinh){
+    echo "Xin chào $ten <br> Sinh nam $namsinh";
 }
 
+
+$a = 10;
+if(isset($a)){
+    thongtin("Nguyen Anh Toi",2000);
+}
+//Yêu cầu nghiêm ngặt
+declare(strict_types = 1);
 ?>
