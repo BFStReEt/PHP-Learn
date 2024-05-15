c57a49cc7d2f1fb1980c0e07e7111a62b9a14f99
Nguyen Anh Toi <toidxbp02@gmail.com>
2024-05-15 14:45:16 +0700

    Bài 37 Form

--- Day-1/xuly.php
+++ Day-1/xuly.php
@@ -1,9 +1,12 @@
-<?php
-echo "Hello mọi người !"
-?>
-<form action="index.php" method="post">
-
-    <input type="text" name="dulieu">
-    <input type="submit">
-
+<form action="index.php" method="get" name="form">
+    <label>Usename</label>
+    <input type="name" name="username" id=""><br>
+    <input type="password" name="password" id="">
+    <label>Password</label>
+    <br>
+    <label>Birthday</label>
+    <input type="date">
+    <br>
+    <label>Sex</label>
+    <input type="radio" value="nam" name="gioitinh">
 </form>
