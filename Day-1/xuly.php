46953214f2ea20d359b6b9e4af8e055bc88699b3
Nguyen Anh Toi <toidxbp02@gmail.com>
2024-05-16 09:07:21 +0700

    Bài 37 Form

--- Day-1/xuly.php
+++ Day-1/xuly.php
@@ -1,12 +1,28 @@
 <form action="index.php" method="get" name="form">
     <label>Usename</label>
     <input type="name" name="username" id=""><br>
-    <input type="password" name="password" id="">
     <label>Password</label>
+    <input type="password" name="password" id="">
+
     <br>
     <label>Birthday</label>
     <input type="date">
     <br>
     <label>Sex</label>
-    <input type="radio" value="nam" name="gioitinh">
+    <input type="radio" value="nam" name="gioitinh">Male
+    <input type="radio" value="nu" name="gioitinh">Female
+    <br>
+    <label>Are you agree ?</label>
+    <input type="checkbox" name="check" value="yes">Yes
+    <input type="checkbox" name="check" value="no">No
+    <br>
+    <label>Quốc tịch</label>
+    <select name="quoctich" id="">
+        <option value="vietnam">Vietnam</option>
+        <option value="my">American</option>
+        <option value="china">China</option>
+    </select>
+    <br>
+    <label for="">Comment</label>
+    <textarea name="" id=""></textarea>
 </form>
