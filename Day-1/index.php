e827bcb03cce9db925a52f1f52d21d0c69980a4f
Nguyen Anh Toi <toidxbp02@gmail.com>
2024-05-16 09:58:13 +0700

    Bài 39 Gửi dữ liệu form nếu có 2 giới tính thì tạo ra name = giới tính valu = nam/nu

--- Day-1/index.php
+++ Day-1/index.php
@@ -1,5 +1,7 @@
 <form action="xuly.php" method="get">
     Name: <input type="text" name="name" id="">
     Gmail: <input type="email" name="email" id="">
-    <input type="submit">
+    <input type="radio" name="gioitinh" value="nu">Nữ
+    <input type="radio" name="gioitinh" value="nu">Nam
+    <input type="submit" value="Gửi">
 </form>
--- Day-1/xuly.php
+++ Day-1/xuly.php
@@ -1,6 +1,9 @@
 <?php
 echo $_GET['name'];
+echo '<br>';
 echo $_GET['email'];
+echo '<br>';
+echo $_GET['gioitinh'];
 ?>
 
 <form action=""></form>
