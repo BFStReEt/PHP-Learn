695278c301f1c6e59f882095e26659de9db5c4de
Nguyen Anh Toi <toidxbp02@gmail.com>
2024-05-16 10:46:43 +0700

    Bài 40 điều kiện FORMif else khi người gửi để trống hiển thị echo ra không để trống

--- Day-1/index.php
+++ Day-1/index.php
@@ -1,7 +1,33 @@
-<form action="xuly.php" method="get">
-    Name: <input type="text" name="name" id="">
-    Gmail: <input type="email" name="email" id="">
-    <input type="radio" name="gioitinh" value="nu">Nữ
-    <input type="radio" name="gioitinh" value="nu">Nam
-    <input type="submit" value="Gửi">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php
+    $nameempty = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST['name'])) {
+            $nameempty = "Hong được để trống bé ơi !";
+        } else {
+            echo $_POST['name'];
+        }
+    }
+
+    ?>
+    <form action="index.php" method="post">
+        Tên: <input type="text" name="name">
+        <span class="error">
+            <?php echo $nameempty; ?>
+        </span>
+        <br>
+        <input type="submit" name="submit">
+    </form>
+</body>
+
+</html>
--- Day-1/xuly.php
+++ Day-1/xuly.php
@@ -1,9 +1,5 @@
 <?php
-echo $_GET['name'];
-echo '<br>';
-echo $_GET['email'];
-echo '<br>';
-echo $_GET['gioitinh'];
+echo $_POST['name'];
 ?>
 
 <form action=""></form>
