3deea7045854a554318c13372de2c364e7889ab3
Nguyen Anh Toi <toidxbp02@gmail.com>
2024-05-16 09:18:14 +0700

    Bài 39 Lấy dữ liệu form bằng get và post

--- Day-1/index.php
+++ Day-1/index.php
@@ -1,4 +1,5 @@
-
-<?php
-header('location: xuly.php')
-?>
+<form action="xuly.php" method="get">
+    Name: <input type="text" name="name" id="">
+    Gmail: <input type="email" name="email" id="">
+    <input type="submit">
+</form>
--- Day-1/xuly.php
+++ Day-1/xuly.php
@@ -1,28 +1,6 @@
-<form action="index.php" method="get" name="form">
-    <label>Usename</label>
-    <input type="name" name="username" id=""><br>
-    <label>Password</label>
-    <input type="password" name="password" id="">
+<?php
+echo $_GET['name'];
+echo $_GET['email'];
+?>
 
-    <br>
-    <label>Birthday</label>
-    <input type="date">
-    <br>
-    <label>Sex</label>
-    <input type="radio" value="nam" name="gioitinh">Male
-    <input type="radio" value="nu" name="gioitinh">Female
-    <br>
-    <label>Are you agree ?</label>
-    <input type="checkbox" name="check" value="yes">Yes
-    <input type="checkbox" name="check" value="no">No
-    <br>
-    <label>Quốc tịch</label>
-    <select name="quoctich" id="">
-        <option value="vietnam">Vietnam</option>
-        <option value="my">American</option>
-        <option value="china">China</option>
-    </select>
-    <br>
-    <label for="">Comment</label>
-    <textarea name="" id=""></textarea>
-</form>
+<form action=""></form>
