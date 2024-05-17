2d3af7752ae2048a7f0b1e99ca49fc2667451a9b
NguyenAnhToi <toidxbp02gmail.com>
2024-05-17 09:49:53 +0700

    Bài 42 Chức năng tìm kiếm đơn giản, else if ()

--- Day-1/index.php
+++ Day-1/index.php
@@ -1,40 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-    <?php
-    $nameempty = "";
-    $name = "";
-    //Đây là phương thức khi ấn submit
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST['name'])) {
-            $nameempty = "Hong được để trống bé ơi !";
-        } else {
-            $name = $_POST['name'];
-            if (!preg_match("/^[a-zA-Z]*$/", $name)) {
-                $nameempty = 'Không được nhập ký tự đặc biệt';
-            } else {
-                echo $name;
-            }
-        }
+<form action="index.php" method="post">
+    Tên:<input type="text" name="name">
+    <input type="submit">
+</form>
+<?php
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $name = $_POST['name'];
+    echo $name;
+    if($name == "Nguyễn Anh Tới"){
+        echo " sinh năm 2000 github";
     }
-
-    ?>
-    <form action="index.php" method="post">
-        Tên: <input type="text" name="name">
-        <span class="error">
-            <?php echo $nameempty; ?>
-        </span>
-        <br>
-        <input type="submit" name="submit">
-    </form>
-</body>
-
-</html>
+    else if($name == "Hùng"){
+        echo " chúng ta của hiện tại";
+    }
+    else{
+        echo " hiện không tồn tại tên này";
+    }
+}
+?>
