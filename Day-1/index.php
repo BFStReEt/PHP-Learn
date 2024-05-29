ffa4724c4fe0d6ea2a094343b00fcb76f3734ff2
NguyenAnhToi <toidxbp02gmail.com>
2024-05-29 09:10:11 +0700

    Bài 45 Phần 2, fread($,filesize())

--- Day-1/index.php
+++ Day-1/index.php
@@ -1,5 +1,6 @@
 <?php
 $file = fopen("file.txt","r") or die ("error"); // Mở file
+echo fread($file,filesize("file.txt")); // Đọc file
 echo fgets($file); //Đọc được 1 dòng; 
 fclose($file); // Đóng file
 
