9b7c4b511d07ce8e40f5963cf91fa5ced0daa2bd
NguyenAnhToi <toidxbp02gmail.com>
2024-05-29 15:28:00 +0700

    Bài 47 Mở rộng kiểm tra email trong file

--- Day-1/index.php
+++ Day-1/index.php
@@ -1,8 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test email</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 <?php
 $file = fopen("file.txt","r") or die ("error"); // Mở file
-while(!feof($file)){
-    echo fgets($file) . "<br>";
+$emailerror = "";
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+if(empty(fgets($file))){
+    $emailerror = "Email does not empty";
 }
+else{
+    if(filter_var(fgets($file),FILTER_VALIDATE_EMAIL)){
+        echo fgets($file);
+    }
+    else{
+        $emailerror = "Email khong dung dinh dang !";
+        echo fgets($file);
+    }
+}
+
+}
+
 fclose($file); // Đóng file
 
 ?>
+<form action="index.php" method="post">
+    Email: <input type="text" name="email">
+    <br>
+    <span class="error">
+        <?php echo $emailerror ?>
+        <?php echo fgets($file) ?>
+    </span>
+    <input type="submit" value="Gửi">
+</form>
+
+</html>
