390656f56b37b4e83b75bf6c002de432df780d42
NguyenAnhToi <toidxbp02gmail.com>
2024-05-29 09:16:55 +0700

    Bài 47 Vòng lặp với file, dùng while(!feof($)){} để tạo ra vòng lặp

--- Day-1/index.php
+++ Day-1/index.php
@@ -1,7 +1,8 @@
 <?php
 $file = fopen("file.txt","r") or die ("error"); // Mở file
-echo fread($file,filesize("file.txt")); // Đọc file
-echo fgets($file); //Đọc được 1 dòng; 
+while(!feof($file)){
+    echo fgets($file) . "<br>";
+}
 fclose($file); // Đóng file
 
 ?>
