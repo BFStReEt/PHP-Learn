998ce67eae7e9834d6bbb8565772e0ed6927c6f9
Nguyen Anh Toi <toidxbp02@gmail.com>
2024-05-31 10:30:02 +0700

    Bài 54: Giới hạn upload file, dùng else if kiểm tra dung lượng file

--- Main/xuly.php
+++ Main/xuly.php
@@ -4,6 +4,8 @@ if (isset($_POST['btn'])) {
     if (isset($_FILES['hinhanh'])) {
         if ($_FILES['hinhanh']['size'] == 0) {
             echo "Hinh anh khong ton tai";
+        } else if ($_FILES['hinhanh']['size'] > 20) {
+            echo "File ảnh quá lớn vui lòng chọn file ảnh nhỏ hơn !";
         } else {
             move_uploaded_file($_FILES['hinhanh']['tmp_name'], './img/' . $_FILES['hinhanh']['name']);
             echo "Upload file thành công !";
