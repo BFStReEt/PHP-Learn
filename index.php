e1a92fb5ccee71b89a497d7b35fdf33dd1a9ab89
Nguyen Anh Toi <toidxbp02@gmail.com>
2024-05-31 14:47:43 +0700

    Bài 58: Tạo session, dùng session_start() để khởi tạo session, sau đó gán [] =

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 
 <?php
-$cookie = "user";
-$cookievalue = "Nam";
-
-setcookie($cookie, $cookievalue, time() - (500), "/");
-//Một ngày bằng 86400s nhân với 30 ngày
-if (isset($_COOKIE[$cookie])) {
-    echo "Cookie " . $cookie . " da ton tai !";
-} else {
-    echo "Cookie " . $cookie . " khong ton tai xxx !";
-}
-
+session_start();
+$_SESSION["name"] = "HEHE";
 ?>
