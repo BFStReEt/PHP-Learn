7726d12f4bad982a84a507fb81686a690e3f0614
Nguyen Anh Toi <toidxbp02@gmail.com>
2024-05-31 14:41:18 +0700

    Bài 57: Xóa cookie, trừ đi thời gian còn lại của cookie

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 $cookie = "user";
 $cookievalue = "Nam";
 
-setcookie($cookie, $cookievalue, time() + (30), "/");
-
+setcookie($cookie, $cookievalue, time() - (500), "/");
+//Một ngày bằng 86400s nhân với 30 ngày
 if (isset($_COOKIE[$cookie])) {
     echo "Cookie " . $cookie . " da ton tai !";
 } else {
