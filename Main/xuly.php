51b2b53040b28538435b10a12f7df13d8118a907
Nguyen Anh Toi <toidxbp02@gmail.com>
2024-05-31 10:16:03 +0700

    Bài 52:Upload file lên thư mục, dùng isset[post] để bắt sự kiện btn, sau đó dùng if else nếu có hình ảnh thì dùng move_uploaded_file để update file vào thư mục img

--- Main/xuly.php
+++ Main/xuly.php
@@ -1,5 +1,15 @@
-<?php
 
-$a = $_FILES["hinhanh"];
-print_r($a);
+<?php
+if (isset($_POST['btn'])) {
+    if (isset($_FILES['hinhanh'])) {
+        if ($_FILES['hinhanh']['size'] == 0) {
+            echo "Hinh anh khong ton tai";
+        } else {
+            move_uploaded_file($_FILES['hinhanh']['tmp_name'], './img/' . $_FILES['hinhanh']['name']);
+            echo "Upload file thành công !";
+        }
+    } else {
+        echo "Upload file bi loi !";
+    }
+}
 ?>
