ddababa66e0816f25dfdac52aa0c40c957169d74
NguyenAnhToi <toidxbp02gmail.com>
2024-06-01 09:05:06 +0700

    Bài 63: Lọc số nguyên, dùng filter_var(songuyen,FILTER_VALIDATE_INT) == false để lọc

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
-$name = "hoang$%#%# nam";
-$locchuoi = filter_var($name,FILTER_SANITIZE_STRING);
+$songuyen = "ahaha";
+$chuoi = 0;
 
-echo $locchuoi
+$locchuoi = filter_var($chuoi,FILTER_SANITIZE_STRING);
+if(filter_var($songuyen,FILTER_VALIDATE_INT) == false){
+    echo "Day khong phai la so nguyen";
+}
+else{
+    echo $songuyen . " lai la DG House";
+}
+
+if($locchuoi){
+    echo "<br>$locchuoi" . " day la chuoi";
+}
+
+
 ?>
