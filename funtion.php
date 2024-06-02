585350c07643f845d28465d5f485ad379a13515e
Nguyen Anh Toi <toidxbp02@gmail.com>
2024-06-02 11:40:28 +0700

    Bài 67: Viết funtion, dùng funtion để viết, sau đó include để gọi chức năng đó ra

---
+++ funtion.php
@@ -0,0 +1,9 @@
+
+<?php
+function phepcong($a, $b)
+{
+    $c = $a + $b;
+    return $c;
+}
+
+?>
--- index.php
+++ index.php
@@ -1,8 +1,5 @@
-<?php
-$url = "https://github.com/BFStReEt/PHP-Learn/commits/main/";
 
-if (!filter_var($url, FILTER_VALIDATE_URL) == false) {
-    echo "Day la url hop le !";
-} else {
-    echo "Khong la url hop le !";
-}
+<?php
+include("funtion.php");
+echo phepcong(10, 39);
+?>
