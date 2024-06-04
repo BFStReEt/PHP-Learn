f58464e14be31393d7549c36e8734a24fd94c8a3
NguyenAnhToi <toidxbp02gmail.com>
2024-06-04 16:57:18 +0700

    Luyện tập LOGIN,LOGOUT: push

--- register.php
+++ register.php
@@ -10,7 +10,11 @@
         $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
         $sql_check = "SELECT COUNT(*) FROM users WHERE username = ?";
-        $stmt_check =   $conn->prepare($sql_check);
+        $stmt_check = $conn->prepare($sql_check);
+        $stmt_check->bind_param("s",$username);
+        $stmt_check->excute();
+        $stmt_check->bind_result($count);
+
         if(){
             echo "Tài khoản này đã có người dùng";
         }
