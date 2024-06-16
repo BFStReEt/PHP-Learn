9b7b706dfe2f8b186ea1db0ce4d480d8bc1cf0f8
NguyenAnhToi <toidxbp02gmail.com>
2024-06-16 10:31:09 +0700

    Luyện tập LOGIN,LOGOUT: Trong file JS, tạo ra 2 sự kiện, người dùng nhấn nút X và người dùng nhấn bên ngoài thanh thông báo thì modal.style.display = none, ở nhấn bên ngoài thanh thông báo, window.onclick gắn bằng event sau đó nếu sự kiện onclick ngoài thanh thống báo thì đóng lại

--- js/success_message.js
+++ js/success_message.js
@@ -1,6 +1,19 @@
 // JS phần hiển thị thông báo khi đăng ký thành công !
 //Lấy biến modal trong thanh thông báo
 var modal = document.getElementById("successModal");
-var span = document.getElementById("close");
+var span = document.getElementById("close")[0];//[0] = là phần tử đầu tiên của close trong trang
 var confirmButton = document.getElementById("confirmButton");
+
+//Người dùng ấn nút X
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+//Người dùng click vào bên ngoài thanh thông báo
+window.onclick = function (event) {
+    if (event.target == modal) {
+        //Bắt sự kiện nếu click ngoài thanh thông báo
+        modal.style.display = "none";
+    }
+}
 
