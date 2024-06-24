28bf568126a011ff59a900fdc6b63f7be70f8880
Nguyen Anh Toi <toidxbp02@gmail.com>
2024-06-24 22:11:00 +0700

    Chỉnh sửa lại các biến trong config để dùng .env

--- config/config.php
+++ config/config.php
@@ -3,14 +3,16 @@
 require_once __DIR__ . '/../vendor/autoload.php'; //DIR trả về đường dẫn thư mục hiện tại
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+//Tạo ra một đối tượng dotenv, biến createImmutable giúp các biến môi trường không thể bị thay đổi sau khi chúng tải
 $dotenv->load();
+// Tải các biến môi trường và lưu chúng vào các biến môi trường của PHP, cho phép truy cập thông qua 'getenv()' hoặc '$_ENV'
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "login";
+$host = $_ENV['DB_HOST'];
+$db = $_ENV['DB_DATABASE'];
+$user = $_ENV['DB_USERNAME'];
+$pass = $_ENV['DB_PASSWORD'];
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $db, $user, $pass);
 
 if ($conn->connect_error) {
     die("Kết nối thất bại " . $conn->connect_error);
